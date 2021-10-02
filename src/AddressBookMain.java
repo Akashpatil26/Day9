fab6fa82eeae0d6e28cb28887d2aa065a414100d
Akash Patil <akashpatilsrt@gmail.com>
2021-10-02 15:32:16 +0530

    Added : UC1

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class AddressBookMain {
 
 
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book program ");
+        System.out.println(" 1. Add contact. ");
+        System.out.println(" 2. Edit contact. ");
+        System.out.println(" 3. Delete Contact. ");
+    }
+    public static void createContact(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First Name: ");
+        String firstName = scanner.next();
+        System.out.println("Enter Last Name: ");
+        String lastName = scanner.next();
+        System.out.println("Enter email: ");
+        String email = scanner.next();
+        System.out.println("Enter Address: ");
+        String address = scanner.next();
+        System.out.println("Enter Mobile Number");
+        int number = scanner.nextInt();
+
 
     }
 }
